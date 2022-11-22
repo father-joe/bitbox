81f5c943dba6f71037a2a4c59ed4939897b787d7
Johannes V <vater.jo@gmail.com>
2022-11-22 09:15:34 +0100

    Testcode for SFML

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace bitbox
+{
+    class Program
+    {
+        private static RenderWindow _window;
+
+        public static void Main(String[] args)
+        {
+            _window = new RenderWindow(new VideoMode(800, 600), "SFML window");
+            _window.SetVisible(true);
+            _window.Closed += new EventHandler(OnClosed);
+            while (_window.IsOpen)
+            {
+                _window.DispatchEvents();
+                _window.Clear(Color.Red);
+                _window.Display();
+             }
+        }
+
+        private static void OnClosed(Object sender, EventArgs e)
+        {
+            _window.Close();
+        }
+    }
+}
