426f5ae421468c982fd485df98de926c4afe84db
Johannes V <vater.jo@gmail.com>
2022-11-26 16:06:48 +0100

    Add player to Space Invaders

--- SpaceInvaders/SpaceInvadersGame.cs
+++ SpaceInvaders/SpaceInvadersGame.cs
@@ -12,15 +12,23 @@ namespace bitbox
     class SpaceInvadersGame
     {
         private static RenderWindow _window;
+        public static Vector2i windowSize = new Vector2i(1920, 1080);
+
+        SpaceInvadersPlayer player = new SpaceInvadersPlayer();
+
         public void run()
         {
-            _window = new RenderWindow(new VideoMode(1920, 1080), "Space Invaders");
+            _window = new RenderWindow(new VideoMode((uint)windowSize.X, (uint)windowSize.Y), "Space Invaders");
             _window.SetVisible(true);
             _window.Closed += new EventHandler(OnClosed);
             while (_window.IsOpen)
             {
                 _window.DispatchEvents();
-                _window.Clear(Color.Black);                
+                _window.Clear(Color.Black);
+
+                player.PlayerControls();
+                DrawPlayer(ref player);
+
                 _window.Display();
             }
         }
@@ -28,6 +36,11 @@ namespace bitbox
         private static void OnClosed(Object sender, EventArgs e)
         {
             _window.Close();
+        }
+
+        private void DrawPlayer(ref SpaceInvadersPlayer player)
+        {
+            _window.Draw(player.playerRect);
         }
     }
 }
--- SpaceInvaders/SpaceInvadersPlayer.cs
+++ SpaceInvaders/SpaceInvadersPlayer.cs
@@ -3,14 +3,49 @@
 ** Playerclass for the Space Invaders Game
 **
 ***************************************************/
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
 
 namespace bitbox
 {
     class SpaceInvadersPlayer
     {
+        public Vector2f playerSpeed = new Vector2f(0, 0);
+        public RectangleShape playerRect = new RectangleShape(new Vector2f(100, 50));
+        public bool isDead = false;
+
+        public void PlayerControls()
+        {
+            if (Keyboard.IsKeyPressed(Keyboard.Key.D))
+            {
+                playerSpeed.X = 3;
+            }
+            else if (Keyboard.IsKeyPressed(Keyboard.Key.A))
+            {
+                playerSpeed.X = -3;
+            }
+            else
+            {
+                playerSpeed.X = 0;
+            }
+
+            updatePlayer();
+        }
+
+        private void updatePlayer()
+        {
+            if (!(playerRect.Position.X < 0 && playerSpeed.X < 0) && !((playerRect.Position.X + playerRect.Size.X) > SpaceInvadersGame.windowSize.X && playerSpeed.X > 0))
+            {
+                playerRect.Position = new Vector2f(playerRect.Position.X + playerSpeed.X, playerRect.Position.Y + playerSpeed.Y);
+            }
+        }
+
         public SpaceInvadersPlayer()
         {
-
+            playerRect.Position = new Vector2f(playerRect.Position.X + SpaceInvadersGame.windowSize.X/2 - playerRect.Size.X/2, SpaceInvadersGame.windowSize.Y - (int)(playerRect.Size.Y*1.5));
+            // Texture playerTexture
+            // playerRect.Texture = playerTexture
         }
     }
 }
