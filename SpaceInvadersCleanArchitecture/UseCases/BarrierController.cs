a9a642c82a5c792746676db7ecdbe34a7ddd01fb
Johannes V <vater.jo@gmail.com>
2023-05-25 21:46:36 +0200

    .

--- SpaceInvadersCleanArchitecture/UseCases/BarrierController.cs
+++ SpaceInvadersCleanArchitecture/UseCases/BarrierController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Numerics;
-using SFML.Graphics;
-using SFML.System;
+//using SFML.Graphics;
+//using SFML.System;
+//using System.Numerics;
 using bitbox.SpaceInvadersCleanArchitecture.Entitys;
 
 namespace bitbox.SpaceInvadersCleanArchitecture.UseCases
@@ -10,15 +11,22 @@ namespace bitbox.SpaceInvadersCleanArchitecture.UseCases
         private IBarrier barrier;
         private List<Projectile> projectiles;
 
-        private RectangleShape _barrierRect = new RectangleShape(new Vector2f(120, 70));
+        //private RectangleShape _barrierRect = new RectangleShape(new Vector2f(120, 70));
 
-        public RectangleShape barrierRect {get {return _barrierRect;}}
+        //public RectangleShape barrierRect {get {return _barrierRect;}}
 
-        public BarrierController(int position)
+        private Vector2 _position = new Vector2();
+        public Vector2 position { get { return _position; } }
+        private Vector2 _size = new Vector2(120, 70);
+        public Vector2 size { get { return _size; } }
+
+        public BarrierController() { }
+        public BarrierController(int position2)
         {
-            barrier = new SBarrier(position);
+            barrier = new SBarrier(position2);
             projectiles = new List<Projectile>();
-            _barrierRect.Position = new Vector2f(_barrierRect.Size.X/2 + (_barrierRect.Size.X*2) * barrier.BarrierPosition, 600);
+            //_barrierRect.Position = new Vector2f(_barrierRect.Size.X/2 + (_barrierRect.Size.X*2) * barrier.BarrierPosition, 600);
+            _position = new Vector2(_size.X/2 + (_size.X*2) * barrier.BarrierPosition, 600);
             // Weitere Initialisierungen
         }
     }
