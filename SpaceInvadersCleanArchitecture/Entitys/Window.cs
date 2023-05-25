9704dabb1f067ee9dc7dd297ac1986e8b6d9fdc4
fabiand35 <fabian.droll@web.de>
2023-05-25 20:22:10 +0200

    add entity window + interface

---
+++ SpaceInvadersCleanArchitecture/Entitys/Window.cs
@@ -0,0 +1,15 @@
+namespace bitbox.SpaceInvadersCleanArchitecture.Entitys;
+
+public class Window:IWindow
+{
+    public int height { get; }
+
+    public int width { get; }
+
+    public Window(int height, int width)
+    {
+        this.height = 1920 / 2;
+        this.width = (int)((double)1080 / (double)1.2);
+    }
+    
+}
